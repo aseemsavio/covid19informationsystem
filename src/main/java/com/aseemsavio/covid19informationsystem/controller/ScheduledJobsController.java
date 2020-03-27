72210c9f1f6e12ed63adad0cc83fd9deb220c689
Aseem Savio <aseemsavio3@gmail.com>
2020-03-28 00:09:49 +0530

    health end point bug fixed

--- src/main/java/com/aseemsavio/covid19informationsystem/controller/ScheduledJobsController.java
+++ src/main/java/com/aseemsavio/covid19informationsystem/controller/ScheduledJobsController.java
@@ -27,7 +27,7 @@ public class ScheduledJobsController {
      *
      * @throws MalformedURLException
      */
-    @Scheduled(cron = "0 */1 * * *")
+    @Scheduled(cron = "0 0/59 * * * *")
     public void updateCoronaDataToDatabase() throws MalformedURLException {
         scheduledJobs.updateCoronaDataInCache();
     }
